f35d9424d64b477b7d44f64e671f3188c73d4a55
Jeremy Oglesby <iaminaband@gmail.com>
2018-07-31 20:35:16 -0700

    Stateless Scroller working

--- src/Scroller/Scroller.js
+++ src/Scroller/Scroller.js
@@ -4,25 +4,21 @@ import creating from '../Content/CreatingContent.js';
 import { Map } from 'core-js';
 
 class Scroller extends Component {
-  state = { 
-    display: this.props.display,
-    content: []
-  }
 
   render() {
     const display = this.props.display;
     const data = new Map()
       .set('learning', learning)
       .set('creating', creating);
+    const content = display === "" ? "" :
+      data.get(display);
 
-    this.state.content = data.get(display);
-
-    if (this.state.content) {
+    if (content) {
       return(
         <div>
           <div>{this.props.display}</div>
-          <h1>{this.state.content.heading}</h1>
-          <p>{this.state.content.text}</p>
+          <h1>{content[0].heading}</h1>
+          <p>{content[0].text}</p>
         </div>
       )
     } else {
